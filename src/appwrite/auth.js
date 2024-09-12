5340493b5819827c0c1971c09297cb7b21913bd2
hexaalif <shadullahsad9@gmail.com>
2024-09-12 23:20:30 +0600

    auth of appwrite added

---
+++ src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import { Account, Client, ID } from "appwrite";
+import conf from "../config/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //  call anouther account
+        return await this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
